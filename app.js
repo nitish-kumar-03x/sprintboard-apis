d51308fe336474bf0e1910a0fb7ab122cd231e6a
Nitish Kumar <nk9990207902@gmail.com>
2026-03-24 13:37:12 +0530

    db setup

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(8000,()=>{
-    console.log('http://localhost:8000');
-})
+const server = async () => {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("DB has been connected");
+  app.listen(process.env.PORT, () => {
+    console.log(`http://${process.env.HOST}:${process.env.PORT}`);
+  });
+};
+
+server();
