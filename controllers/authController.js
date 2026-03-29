8590076763c863e51bfdb1c5535d92746ecc2306
Nitish Kumar <nk9990207902@gmail.com>
2026-03-29 22:46:06 +0530

    login api done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
-const user = require("../models/user");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+// REGISTER
 const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
@@ -11,9 +13,9 @@ const register = async (req, res) => {
 
     const normalizedEmail = email.toLowerCase();
 
-    const isEmailMatched = await user.findOne({ email: normalizedEmail });
+    const existingUser = await User.findOne({ email: normalizedEmail });
 
-    if (isEmailMatched) {
+    if (existingUser) {
       return res.status(400).json({
         message: "Email already registered",
       });
@@ -21,7 +23,7 @@ const register = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await user.create({
+    const newUser = await User.create({
       name,
       email: normalizedEmail,
       password: hashedPassword,
@@ -30,9 +32,13 @@ const register = async (req, res) => {
 
     res.status(201).json({
       message: "User registered successfully",
-      data: {name,email,role},
+      data: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
     });
-
   } catch (error) {
     res.status(500).json({
       message: "Server error",
@@ -41,4 +47,62 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+// LOGIN
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const normalizedEmail = email.toLowerCase();
+
+    const foundUser = await User.findOne({ email: normalizedEmail });
+
+    if (!foundUser) {
+      return res.status(400).json({
+        message: "User Not Found",
+      });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      foundUser.password,
+    );
+
+    if (!isPasswordMatched) {
+      return res.status(400).json({
+        message: "Email or Password is Incorrect",
+      });
+    }
+
+    const payload = {
+      id: foundUser._id,
+      email: foundUser.email,
+      role: foundUser.role,
+    };
+
+    const stoken = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      stoken,
+      data: {
+        id: foundUser._id,
+        name: foundUser.name,
+        email: foundUser.email,
+        role: foundUser.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { register, login };
