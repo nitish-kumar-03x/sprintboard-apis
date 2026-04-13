82687d4201bc4dc8fe5c837f62416fa589634506
Nitish Kumar <nk9990207902@gmail.com>
2026-04-13 13:06:50 +0530

    APIs & Task Router

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const morgan = require('morgan');
- 
+const morgan = require("morgan");
+
 dotenv.config();
 
- 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
 const authRouter = require("./routes/auth");
- 
+const tasksRouter = require("./routes/tasks");
+
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 8000;
 const MONGO_URL = process.env.MONGO_URL;
- 
+
 // Routes
 app.use("/api/public/auth", authRouter);
- 
+app.use("/api/private/tasks", tasksRouter);
+
 const startServer = async () => {
   try {
     await mongoose.connect(MONGO_URL);
     console.log("Database connected");
- 
+
     app.listen(PORT, () => {
       console.log(`Server is listening on http://${HOST}:${PORT}`);
     });
@@ -31,5 +32,5 @@ const startServer = async () => {
     console.error(error);
   }
 };
- 
+
 startServer();
