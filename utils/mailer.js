dc1952416ac3a97db342720a1cf70c679732c1d9
Nitish Kumar <nk9990207902@gmail.com>
2026-06-16 13:08:02 +0530

    having port and host for email

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,7 +6,10 @@ dns.setDefaultResultOrder('ipv4first');
 const sendLoginNotification = async (email, name) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: process.env.EMAIL_SERVICE || 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
